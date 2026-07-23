4b24b57f376d4220ceed276e4596e07b3c4dd8b0
MrBanana <logande4610@gmail.com>
2026-07-23 18:10:20 -0500

    Add project files.

---
+++ ATT_Hardcore/HardcoreManager.cs
@@ -0,0 +1,131 @@
+﻿using Alta.Api.DataTransferModels.Models.Responses;
+using Alta.Character;
+using Alta.Networking;
+using Alta.Utilities;
+using HarmonyLib;
+using MelonLoader;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace ATT_Hardcore {
+    internal class HC_Json : IAltaFileFormat/*, IAutoSave*/ {
+        /// <summary>
+        /// string = IpAddress
+        /// 
+        /// <br></br>
+        /// 
+        /// int = playerId
+        /// </summary>
+        internal Dictionary<string, int> deadPlayerIds = new Dictionary<string, int>();
+
+        //public HC_Json() {
+        //    StreamerManager.AutoSavingBehaviours.Add(this);
+        //}
+
+        //public void AutoSave() {
+        //    HardcoreManager.Save();
+        //}
+
+        public override IAltaFileFormat Clone() {
+            HC_Json clone = new HC_Json();
+
+            clone.deadPlayerIds = new Dictionary<string, int>(deadPlayerIds);
+
+            return clone;
+        }
+
+        public override void ReadFrom(FileInfo info, Stream stream) {
+            using BinaryReader reader = new BinaryReader(stream);
+
+            int arrayCount = reader.ReadInt32();
+
+            deadPlayerIds.Clear();
+
+            for (int i = 0; i < arrayCount; i++) {
+                string IpAddress = reader.ReadString();
+                int playerId = reader.ReadInt32();
+
+                deadPlayerIds.Add(IpAddress, playerId);
+            }
+        }
+
+        public override void WriteTo(Stream stream, CancellationToken token) {
+            using BinaryWriter writer = new BinaryWriter(stream);
+
+            writer.Write(deadPlayerIds.Count);
+
+            foreach (var pair in deadPlayerIds) {
+                writer.Write(pair.Key);
+                writer.Write(pair.Value);
+            }
+        }
+    }
+
+    public static class HardcoreManager {
+        private static IAltaFile hardcoreFile;
+
+        private static IAltaFolder savesFolder;
+
+        internal static HC_Json data { get; private set; }
+
+        internal static bool isInitialized { get; private set; }
+
+        private static bool PassesChecks() {
+            return !NetworkSceneManager.IsServer && !isInitialized;
+        }
+
+        internal static async void Init(GameServerInfo server) {
+            if (!NetworkSceneManager.IsServer)
+                return;
+
+            IAltaFolder serverFolder = AltaIO.ServersFolder;
+            IAltaFolder serverIdFolder = serverFolder.GetSubfolder(server.Identifier.ToString());
+            savesFolder = serverIdFolder.GetSubfolder("Save");
+
+            hardcoreFile = savesFolder.GetFile("DeadPlayers.bytes");
+
+            data = await hardcoreFile.ReadAsync<HC_Json>();
+
+            if (data is null) {
+                data = new HC_Json();
+
+                hardcoreFile.Content = data;
+            }
+
+            isInitialized = true;
+        }
+
+        // another one bites the dust
+        internal static void PlayerDied(HealthObject healthObject) {
+            if (!PassesChecks())
+                return;
+
+            Player player = healthObject.Entity.Parent.gameObject.GetComponent<PlayerCharacter>().NetworkPlayer;
+
+            Connection connection = player.ConnectionToRemotePlayer;
+
+            string IpAddress = (string)Traverse.Create(connection).Field("IpAddress").GetValue();
+
+            // MrEvil
+            if (string.IsNullOrWhiteSpace(IpAddress)) {
+                IpAddress = "127.0.0.1";
+            }
+
+            data.deadPlayerIds.Add(IpAddress, (int)player.ConnectionToRemotePlayer.UserInfo.Identifier);
+
+            connection.Disconnect("You died.");
+
+            connection.Socket.DestroyConnection(connection, true);
+
+            Save();
+        }
+
+        internal static void Save() {
+            if (!PassesChecks())
+                return;
+
+            hardcoreFile?.WriteAsync();
+        }
+    }
+}
---
+++ ATT_Hardcore/Main.cs
@@ -0,0 +1,71 @@
+﻿using Alta.Api.Client.HighLevel;
+using Alta.Networking;
+using Alta.Networking.Servers;
+using HarmonyLib;
+using MelonLoader;
+using System;
+using System.Reflection;
+
+[assembly: MelonInfo(typeof(ATT_Hardcore.Main), "Hardcore", "1.0.0", "MrDuckTheFifth")]
+
+namespace ATT_Hardcore {
+    public class Main : MelonMod { }
+
+    [HarmonyPatch(typeof(Player), "LinkPlayerControllerToPlayer", new Type[] { typeof(PlayerController), typeof(bool) })]
+    public static class PlayerPatch {
+        public static void Postfix(Player __instance) {
+            __instance.PlayerCharacter.Health.Destroyed.Invoked += HardcoreManager.PlayerDied;
+        }
+    }
+
+    [HarmonyPatch(typeof(ServerHandler), MethodType.Constructor, new Type[] { typeof(ISocket), typeof(IServerAccess), typeof(bool) })]
+    public static class ServerHandlerPatch {
+        public static void Postfix(ServerHandler __instance) {
+            HardcoreManager.Init(__instance.ServerInfo);
+        }
+    }
+
+    [HarmonyPatch(typeof(ServerPlayerConnectionHandlerOld), "CheckIfPlayerIsAllowedCustom", new Type[] { typeof(Connection), typeof(int), typeof(string), typeof(string), typeof(PlayerMode), typeof(PlatformTarget) })]
+    public static class DenyDeadPatch {
+        public static readonly Type ResultType = AccessTools.TypeByName("Alta.Networking.Servers.ServerPlayerConnectionHandlerOld+PlayerJoinResult");
+
+        public static readonly MethodInfo CreateDenied = AccessTools.Method(ResultType, "CreateDeniedResult");
+
+        public static bool Prefix(ref object __result, Connection connection) {
+            object createDenied(string custom = null) {
+                string reason = custom != null ? custom : "You died.";
+
+                object customDeniedResult = CreateDenied.Invoke(null, new object[] { reason });
+
+                MelonLogger.Msg("Dead player tried to join server. Denying access...");
+
+                return customDeniedResult;
+            }
+
+            if (!HardcoreManager.isInitialized) {
+                __result = createDenied("Server hasn't properly finished reading hardcore data. Please wait.");
+
+                return false;
+            }
+
+            var list = HardcoreManager.data.deadPlayerIds;
+
+            if (list.Count <= 0)
+                return true;
+
+            string IpAddress = (string)Traverse.Create(connection).Field("IpAddress").GetValue();
+
+            if (string.IsNullOrWhiteSpace(IpAddress)) {
+                IpAddress = "127.0.0.1";
+            }
+
+            if (list.ContainsKey(IpAddress) || list.ContainsValue(connection.UserInfo.Identifier)) {
+                __result = createDenied();
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
